e06587adb1a785354faf888468cceb8b7a4c336c
viewpointics-tan <viewpointicstan@gmail.com>
2021-11-16 05:12:39 +0000

    add: Cell.tsx を追加した

---
+++ components/molecules/Cell.tsx
@@ -0,0 +1,10 @@
+import { Textarea } from "@chakra-ui/textarea";
+import { Box } from "@chakra-ui/layout";
+
+const Cell = (): JSX.Element => (
+  <Box>
+    <Textarea></Textarea>
+  </Box>
+);
+
+export default Cell;
