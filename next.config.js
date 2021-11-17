b3ba02e253cd76280d807648c06abf02ebf4c663
viewpointics-tan <viewpointicstan@gmail.com>
2021-11-17 09:41:12 +0000

    fix: webpackの設定を追加し、開発サーバーのエラーを修正した

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.m?js$/,
+      type: 'javascript/auto',
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+    return config;
+  },
+}
