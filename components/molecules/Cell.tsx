ea77d6c3b47ee09a7fcd007c8303c8c07d75247c
viewpointics-tan <viewpointicstan@gmail.com>
2021-11-17 09:42:43 +0000

    update: 文字数を表示できるCellを作った

--- components/molecules/Cell.tsx
+++ components/molecules/Cell.tsx
@@ -1,11 +1,20 @@
 import { Textarea } from "@chakra-ui/textarea";
 import { Box } from "@chakra-ui/layout";
-import { Countable } from "countable";
+import stringLength from "string-length";
+import { useState } from "react";
 
-const Cell = (): JSX.Element => (
-  <Box>
-    <Textarea></Textarea>
-  </Box>
-);
+const Cell = (): JSX.Element => {
+  const [length, updateLength] = useState(0);
+  return (
+    <Box>
+      <Textarea
+        onInput={(e) => {
+          updateLength(stringLength(e.currentTarget.value));
+        }}
+      ></Textarea>
+      <Box>{length}</Box>
+    </Box>
+  );
+};
 
 export default Cell;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+// import Link from 'next/link'
+import Layout from "../components/Layout";
+import Cell from "../components/molecules/Cell";
 
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
+    <Cell></Cell>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
