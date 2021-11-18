1e6273b6d0bf4eee27d2fa335e7fd67eb0b3b895
viewpointics-tan <viewpointicstan@gmail.com>
2021-11-18 00:59:36 +0000

    update: Flex を追加した

--- components/molecules/Cell.tsx
+++ components/molecules/Cell.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@chakra-ui/layout";
+import { Flex } from "@chakra-ui/react";
 import { Textarea } from "@chakra-ui/textarea";
 import { useState } from "react";
 import stringLength from "string-length";
@@ -18,8 +19,10 @@ const Cell: React.VFC<Props> = ({ maxLength }) => {
           updateLength(stringLength(e.currentTarget.value));
         }}
       ></Textarea>
-      {excessLength > 0 && <Box color="red.300">超過:{excessLength}</Box>}
-      <Box>文字数:{length}</Box>
+      <Flex>
+        {excessLength > 0 && <Box color="red.300">超過:{excessLength}</Box>}
+        <Box>文字数:{length}</Box>
+      </Flex>
     </Box>
   );
 };
