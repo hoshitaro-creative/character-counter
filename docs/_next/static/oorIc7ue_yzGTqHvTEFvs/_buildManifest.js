143c86ace4ced773ea2d9917a7b2e494c223c907
viewpointics-tan <viewpointicstan@gmail.com>
2021-11-26 09:25:10 +0000

    update: ビルドした

---
+++ docs/_next/static/oorIc7ue_yzGTqHvTEFvs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/662-e8e9be9e121e070b.js","static/chunks/pages/index-3628b298f740c848.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- docs/_next/static/wM84QWE-niFcOBSkSXlRD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/662-b8d0fa49aa27145b.js","static/chunks/pages/index-94266a1c6be1a415.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
