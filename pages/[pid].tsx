d1f88d39b3526a971f5137193a51c3369d67b6bc
viewpointics-tan <viewpointicstan@gmail.com>
2021-12-01 01:33:52 +0000

    update: ページ番号のアルゴリズムを作った

--- pages/[pid].tsx
+++ pages/[pid].tsx
@@ -6,15 +6,36 @@ const Page = (): JSX.Element => {
   const [configs, setConfigs] = useState<TableConfig[]>([]);
   const router = useRouter();
   const { pid } = router.query;
+  let configNumber = 0;
+
+  const calculateConfigNumber = (pid: string): number => {
+    const configNumber = Number(pid);
+    if (configNumber === 1) {
+      return 1;
+    } else if (1 < configNumber && configNumber % 2 === 0) {
+      return 2;
+    } else if (1 < configNumber && configNumber % 2 === 1) {
+      return 3;
+    } else {
+      return 0;
+    }
+  };
+
+  if (typeof pid == "string") {
+    configNumber = calculateConfigNumber(pid);
+  } else {
+    configNumber = 0;
+  }
+
   useEffect(() => {
-    if (pid === "1" || pid === "2" || pid === "3") {
-      import(`/sample/config${pid.padStart(2, "0")}`).then((module) => {
+    if (1 <= configNumber && configNumber <= 3) {
+      import(`/sample/config0${configNumber}`).then((module) => {
         setConfigs(module.configs);
       });
     } else {
       setConfigs([]);
     }
-  }, [pid]);
+  }, [configNumber]);
   return <div>{configs.length ? JSON.stringify(configs) : "no data"}</div>;
 };
 
