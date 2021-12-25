528380c90c950e26611374877b1e797be54f3b7a
viewpointics-tan <viewpointicstan@gmail.com>
2021-12-25 16:57:50 +0900

    update: ログイン状態による分岐を実装した

--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -5,35 +5,50 @@ import { Box } from "@chakra-ui/react";
 import LogoutButton from "components/atoms/LogoutButton";
 import Layout from "components/Layout";
 import Page from "components/Page";
-import { useState } from "react";
+import app from "firebase";
+import { getAuth, onAuthStateChanged, User } from "firebase/auth";
+import { useEffect, useState } from "react";
 
 const AppPage = () => {
   const [pageNumber, setPageNumber] = useState(1);
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() =>
+    onAuthStateChanged(getAuth(app), (user) => {
+      setUser(user);
+    })
+  );
 
   return (
     <Layout title="character counter app">
-      <Box backgroundColor="red.100" height={100}></Box>
-      <Flex direction="row-reverse">
-        <LogoutButton></LogoutButton>
-      </Flex>
-      <Flex direction="row" justifyContent="center">
-        <Button
-          onClick={() => {
-            setPageNumber(pageNumber - 1);
-          }}
-        >
-          -
-        </Button>
-        <Box>{pageNumber}</Box>
-        <Button
-          onClick={() => {
-            setPageNumber(pageNumber + 1);
-          }}
-        >
-          +
-        </Button>
-      </Flex>
-      <Page pid={pageNumber}></Page>
+      {user ? (
+        <>
+          <Box backgroundColor="red.100" height={100}></Box>
+          <Flex direction="row-reverse">
+            <LogoutButton></LogoutButton>
+          </Flex>
+          <Flex direction="row" justifyContent="center">
+            <Button
+              onClick={() => {
+                setPageNumber(pageNumber - 1);
+              }}
+            >
+              -
+            </Button>
+            <Box>{pageNumber}</Box>
+            <Button
+              onClick={() => {
+                setPageNumber(pageNumber + 1);
+              }}
+            >
+              +
+            </Button>
+          </Flex>
+          <Page pid={pageNumber}></Page>
+        </>
+      ) : (
+        <Box>ログインしていません</Box>
+      )}
     </Layout>
   );
 };
